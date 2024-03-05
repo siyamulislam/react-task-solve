b96ef7b39efcdfc9c0a8a1dfbe5af3880c41072b
siyamulislam <siyamulislam1@gmail.com>
2024-03-05 17:07:03 +0600

    problem2 modal ui

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,6 +1,29 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Modal, Button, Form, FormGroup, FormControl } from 'react-bootstrap';
 const Problem2 = () => {
+    const [modalA, setModalA] = useState(false);
+    const [modalB, setModalB] = useState(false);
+    const [modalC, setModalC] = useState(false);
+
+
+
+    const handleOpenModalA = () => {
+        console.log('DFDFD')
+        setModalA(true);
+        setModalB(false);
+    };
+
+    const handleOpenModalB = () => {
+        setModalB(true);
+        setModalA(false);
+    };
+
+    const handleOpenModalC = () => setModalC(true);
+
+
+    const handleCloseModalA = () => setModalA(false);
+    const handleCloseModalB = () => setModalB(false);
+    const handleCloseModalC = () => setModalC(false);
 
     return (
 
@@ -9,9 +32,66 @@ const Problem2 = () => {
                 <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
                 
                 <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+                <Button className="btn btn-lg btn-primary" onClick={handleOpenModalA}>All Contacts</Button>
+                    <Button className="btn btn-lg btn-warning" onClick={handleOpenModalB}>US Contacts</Button>
                 </div>
+
+
+                <Modal show={modalA} onHide={handleCloseModalA}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>All Contacts  </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+
+                            <FormGroup className="mb-3" controlId="formBasicSearch">
+                                <FormControl type="text" placeholder="Search"  />
+                            </FormGroup>
+                            <div style={{ maxHeight: '300px', overflowY: 'auto' }} >
+                            <div >phone</div> 
+                            <div >phone</div> 
+                            <div >phone</div> 
+                            <div >phone</div> 
+
+                            </div>
+                            <FormGroup className="my-2" controlId="formBasicCheckbox">
+                                <Form.Check type="checkbox" label="Only even"  />
+                            </FormGroup>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="primary" onClick={handleOpenModalA}>All Contacts</Button>
+                        <Button variant="secondary" onClick={handleOpenModalB}>US Contacts</Button>
+                        <Button variant="danger" onClick={handleCloseModalA}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
+                <Modal show={modalB} onHide={handleCloseModalB}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>US Contacts </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+
+                            <FormGroup className="mb-3" controlId="formBasicSearch">
+                                <FormControl type="text" placeholder="Search" />
+                            </FormGroup>
+                            <div style={{ maxHeight: '300px', overflowY: 'auto' }} > 
+                                    <div >phone</div> 
+                                    <div >phone</div> 
+                                    <div >phone</div> 
+                                    <div >phone</div> 
+                            </div>
+                            <FormGroup className="mb-3" controlId="formBasicCheckbox">
+                                <Form.Check type="checkbox" label="Only even"  />
+                            </FormGroup>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="primary" onClick={handleOpenModalA}>All Contacts</Button>
+                        <Button variant="secondary" onClick={handleOpenModalB}>US Contacts</Button>
+                        <Button variant="danger" onClick={handleCloseModalB}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
                 
             </div>
         </div>
